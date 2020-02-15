86dbd08feb046da0ad26726d62b769666f2376a8
bpadhiyar4 <bpadhiyar4@gmail.com>
2020-02-15 22:53:30 +0530

    'final'

--- api.py
+++ api.py
@@ -1,24 +1,29 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
+from flask_cors import CORS
 import joblib
 import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
-
+CORS(app)
 ml_model = joblib.load('ml_model.joblib')
 
 @app.route('/')
-@app.route('/predict')
+@app.route('/predict', methods=['POST'])
 def Post():
     try:
+        data = request.get_json()
+        data = pd.DataFrame(data, index=[0])
+        # print(data)
         response = make_response()
-        data = [57, 0, 0, 2, 120, 163, 0]
+        # data = [57, 0, 0, 2, 120, 163, 0]
         prediction = ml_model.predict(np.array(data).reshape((1,-1)))
         response = jsonify({
             "statusCode": 200,
             "status": "Prediction Made",
-            "result": "Prediction :" + str(prediction[0])
+            "result": str(prediction[0])
         })
-        response.headers.add('Access-Control-Allow-Origin', '*')
+        # response.headers.add('Access-Control-Allow-Origin', '*')
         return response
     except Exception as error:
         return jsonify({
--- model_generator.py
+++ model_generator.py
@@ -6,15 +6,11 @@ from sklearn.metrics import mean_absolute_error
 import joblib
 
 data = pd.read_csv("dataset/heart.csv")
-# print(data.head())
 
 FEATURES = ['age', 'sex', 'cp', 'thal', 'trestbps', 'thalach', 'fbs']
 
 X = data[FEATURES]
 Y = data['target']
-
-# print(X.head())
-# print(Y.head())
 
 X_train, x_test, Y_train, y_test = train_test_split(X,Y, random_state=1)
 
@@ -22,9 +18,8 @@ model = LogisticRegression()
 model.fit(X_train, Y_train)
 
 y_pred = model.predict(x_test)
-# print(y_pred)
 
-# print("Accuracy of Model : {}" .format(mean_absolute_error(y_test, y_pred)))
+accuracy = model.score(x_test, y_test)
 print("Accuracy Of Model :  {}" .format(model.score(x_test, y_test)))
 
 joblib.dump(model, 'ml_model.joblib')
